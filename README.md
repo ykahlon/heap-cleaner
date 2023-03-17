eb883cc7769c08096cd2cf946b0c9682ae61631f
Jan Dolezel <jandolezel@microsoft.com>
2023-03-17 11:17:20 +0100

    Config polishing

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Usage
 
-Just run src/cleaner.ts with the input file and optionally pass in a node id to focus.
+Just run src/bin.ts with the input file and optionally pass in a node id to focus.
 If the node to focus is not provided, the program will try to find a detached window and focus on it.
 
 ```
-node --require ts-node/register --max-old-space-size=32768 --stack-size=320000000 src/cleaner.ts sample/sample.heapsnapshot
+node --require ts-node/register --max-old-space-size=32768 --stack-size=320000000 src/bin.ts sample/sample.heapsnapshot
 ```
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,10 @@ import { extname } from 'path'
 import { GraphManager } from './graph-manager'
 import { type HeapSnapshotWorkerDispatcher } from './vendor/HeapSnapshotWorkerDispatcher'
 import { HeapSnapshotLoader } from './vendor/HeapSnapshotLoader'
-import { log, error } from './log'
+import { log } from './log'
+import { JSHeapSnapshot } from './vendor/HeapSnapshot'
 
-// Reduces the heap snapshot with focus on a node with a given id or if not provided,
-// on a single detached window found in the snapshot.
-const run = async (filePath: string, nodeId: string | undefined) => {
-  log(`reading file ${filePath} - start!`)
+export const loadSnapshot = async (filePath: string) => {
   const readStream = createReadStream(filePath, {
     highWaterMark: 10 * 1024 * 1024,
     encoding: 'utf8',
@@ -31,24 +29,33 @@ const run = async (filePath: string, nodeId: string | undefined) => {
   })
 
   await loaderPromise
-  log('reading file - end!')
-  const snapshot = loader.buildSnapshot()
-  log('reading file - end!')
+  return loader.buildSnapshot()
+}
 
+export const focusOnNode = (snapshot: JSHeapSnapshot, nodeId: string | undefined) => {
   const graphManager = new GraphManager(snapshot)
   const nodeIdToFocus =
-    nodeId === undefined
-      ? (log('Looking for detached window'), graphManager.findNodeByName('Detached Window').getNodeId())
-      : (log('Focusing on node', nodeId), parseInt(nodeId))
+    nodeId === undefined ? graphManager.findNodeByName('Detached Window').getNodeId() : parseInt(nodeId)
   graphManager.focusOnNode(nodeIdToFocus, graphManager.findNodeByName('(GC roots)').getNodeId())
   const jsonOutput = graphManager.exportGraphToJson()
+  return { jsonOutput, nodeIdToFocus }
+}
+
+// Reduces the heap snapshot with focus on a node with a given id or if not provided,
+// on a single detached window found in the snapshot.
+export const run = async (filePath: string, nodeId: string | undefined) => {
+  log(`reading file ${filePath} - start!`)
+  const snapshot = await loadSnapshot(filePath)
+  log('reading file - end!')
+
+  if (nodeId === undefined) {
+    log('Looking for detached window')
+  } else {
+    log('Focusing on node', nodeId)
+  }
+  const { jsonOutput, nodeIdToFocus } = focusOnNode(snapshot, nodeId)
 
   const output = `${filePath.substring(0, filePath.length - extname(filePath).length)}-${nodeIdToFocus}.heapsnapshot`
   await fs.writeFile(output, JSON.stringify(jsonOutput), { encoding: 'utf-8' })
   log(`See output in ${output}`)
 }
-
-const appParams = process.argv.slice(2)
-run(/* filePath */ appParams[0], /* nodeId */ appParams[1])
-  .then(() => log('done'))
-  .catch((err) => error(err))
